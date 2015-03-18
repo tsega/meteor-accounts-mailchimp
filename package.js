757ac1c68b169e3c6604957ea65eaad36d959dc8
Tsegaselassie Tadesse <tsegaselassi@gmail.com>
2015-03-18 19:14:49 +0300

    Added semicolon to state assignment.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Login service for mailchimp accounts",
-  version: "0.0.11",
+  version: "0.0.12",
   git: "https://github.com/tsega/meteor-accounts-mailchimp"
 });
 
