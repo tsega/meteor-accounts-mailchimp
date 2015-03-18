3e4688f236bc227e6adc85558a8c155cc2f8b74f
Tsegaselassie Tadesse <tsegaselassi@gmail.com>
2015-03-18 18:11:28 +0300

    Revered redirect_uri value to original one.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Login service for mailchimp accounts",
-  version: "0.0.9_1",
+  version: "0.0.10",
   git: "https://github.com/tsega/meteor-accounts-mailchimp"
 });
 
